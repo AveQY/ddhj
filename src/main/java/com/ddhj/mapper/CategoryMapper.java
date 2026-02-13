4816052f7befbac48482453a8e55677121d5e38f
AweQY <2049898109@qq.com>
2026-02-13 21:56:12 +0800

    Revert "第一次提交"
    This reverts commit 3b8e703600309c5e5d44bb0c2e410229ca8a62be.

--- src/main/java/com/ddhj/mapper/CategoryMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ddhj.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ddhj.entity.Category;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CategoryMapper extends BaseMapper<Category> {
-}
--- src/main/java/com/ddhj/mapper/SpecificationMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ddhj.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ddhj.entity.Specification;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SpecificationMapper extends BaseMapper<Specification> {
-}
